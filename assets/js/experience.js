3fadeb9219febadb24489f7f544a279049971fcd
datsith <zabala@caltech.edu>
2022-05-13 13:22:14 -0700

    added experience

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,26 +5,57 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Senior Data Scientist",
+    cardImage: "assets/images/experience-page/wgt.jpg",
+    place: "Walmart Global Tech",
+    time: "(Feb 2022 - Mar 2022)",
+    desp: `<li>Combined Neural Nets for Object Tracking with SLAM methods to build a store's planogram</li>
+           <li>Architected End-to-End Machine Learning solutions spanning three product verticals</li>
+           <li>Devised scaling strategies for deploying a CV-based Inventory Tracking System across 4700+ stores</li>`,
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
-  },
-  {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
-  },
+    title: "Deep Learning Researcher",
+    cardImage: "assets/images/experience-page/sn8.jpg",
+    place: "Walmart Store No 8 (Incubator)",
+    time: "(Aug 2021 - Feb 2022)",
+    desp: `<li>Implemented heuristic algorithms for determining On-Shelf Availability of retail products</li>
+           <li>Trained CNN ensembles for Object Detection and Object Tracking in densely packed images</li>
+           <li>Contributed to the *fastest* incubation project to graduate from Store No. 8 into Walmart Global Tech</li>`,
+},
+{
+  title: "Founder",
+  cardImage: "assets/images/experience-page/acrobotic.jpg",
+  place: "ACROBOTIC",
+  time: "(Jun 2013 - Jul 2021)",
+  desp: `<li>Managed a technical apprenticeship program in partnership with two local colleges</li>
+         <li>Launched and successfully delivered 3 hardware products on Kickstarter</li>
+         <li>Created tutorials and demos on YouTube <a href="https://youtube.com/datasith">growing an audience</a> of 30k+ subscribers</li>`,
+},
+{
+  title: "Machine Learning Engineer",
+  cardImage: "assets/images/experience-page/iorodeo.jpg",
+  place: "IO Rodeo",
+  time: "(Jun 2012 - May 2013)",
+  desp: `<li>Manufactured custom lab instrumentation for behavioral studies of fruit flies</li>
+         <li>Developed Control and Human-Machine interfaces for interactive lab instruments</li>
+         <li>Designed custom PCBs and mechanical assemblies using ECAD/CAD software</li>`,
+},
+{
+  title: "PhD Research Scientist, Computer Vision",
+  cardImage: "assets/images/experience-page/caltech.jpg",
+  place: "California Institute of Technology",
+  time: "(Sep 2009 - Aug 2012)",
+  desp: `<li>Developed analysis pipelines for high-speed videography (post-processing and real-time)</li>
+         <li>Applied Unsupervised Learning algorithms to track insect wing and body 3D kinematics</li>
+         <li>Applied Control and Dynamical Systems theory to study sensory-motor transformations</li>`,
+},
+{
+  title: "Professional Soccer Player",
+  cardImage: "assets/images/experience-page/italchacao.jpg",
+  place: "Deportivo Italchacao",
+  time: "(Sep 2009 - Aug 2012)",
+  desp: `<li>Played as Center Forward in the U17 category with some appearances for the first team</li>`,
+},
 ];
 
 const showCards2 = () => {
@@ -62,28 +93,18 @@ document.addEventListener("DOMContentLoaded", showCards2);
 const volunteership = document.querySelector(".volunteership");
 const volunteershipcards = [
   {
-    title: "GirlScript Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/1.jpg",
+    title: "Girls Who Code @San Marino HS",
+    cardImage: "assets/images/experience-page/gwc.jpg",
     description:
-      "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
+      `"As a co-instructor I was responsible for creating and imparting the curriculum,
+        which I based around Physical Computing devices such as Arduino dev boards and Raspberry Pi SBCs."`,
   },
   {
-    title: "StudentCode-in 2020",
-    cardImage: "assets/images/experience-page/2.jpg",
+    title: "DIY Girls",
+    cardImage: "assets/images/experience-page/diygirls.jpg",
     description:
-      "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
-  },
-  {
-    title: "PClub Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/3.jpg",
-    description:
-      "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
-  },
-  {
-    title: "Hakin-Codes",
-    cardImage: "assets/images/experience-page/4.jpg",
-    description:
-      "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
+      `"As a volunteer, I have helped with event logistics and collaborated with the founder, 
+        Luz Rivas, in the creation of curricula around DIY Electronic projects."`,
   },
 ];
 
@@ -106,99 +127,15 @@ const showCards = () => {
 };
 document.addEventListener("DOMContentLoaded", showCards);
 
-// Hackathon Section
+// Open Source Section
 
 const hackathonsection = document.querySelector(".hackathon-section");
 const mentor = [
   {
-    title: "The Uplift Project",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/uplift.png",
-    desp: "I would be responsible for guiding 100s of participants across the globe in their first steps towards Applied Data Science and building end-to-end machine learning projects.",
-  },
-  {
-    title: "ULHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/ulhacks.png",
-    desp: "ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
-  },
-  {
-    title: "WaffleHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/wafflehacks.png",
-    desp: "WaffleHacks 2021 is an upcoming 48-hour student-organized hackathon with an aim to bring technical solutions to local communities and small businesses.",
-  },
-  {
-    title: "Elevate Tech",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/elevate.png",
-    desp: "Elevate Hacks is an upcoming all-female virtual hackathon coming from AUGUST 14th to 15th.",
-  },
-  {
-    title: "PitchTeen",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/pitchteen.png",
-    desp: "PitchTeen 2.0 was a tech startup competition for female and non-binary communities in high school and college from all over the world with an aim to create a safe and encouraging place for women to help them explore tech and business fields.",
-  },
-  {
-    title: "Hack-A-Solution",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/hackasolution.png",
-    desp: "Hack-a-Solution 2021 is a 24 hour long hackathon organized by Frisco students to raise awareness of world issues through technology.",
-  },
-  {
-    title: "UniGlobe Hacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/uniglobe.png",
-    desp: "UniGlobe Hacks 2.0 is an upcoming 4-day long hackathon run by high school students held during 28-31 July 2021.",
-  },
-  {
-    title: "AtlasHacks",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/atlas.png",
-    desp: "AtlasHacks II is a 3-day virtual hackathon that aims to foster creativity within hackers all around the world.",
-  },
-  {
-    title: "NeoHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/neo.png",
-    desp: "NeoHacks is an upcoming 48-hour hackathon taking place July 23-25, 2021 where hundreds of students from around the world will come together virtually to learn new skills, meet new friends, and build awesome projects. We provide beginner-friendly workshops, mentorship, fun games, and more.",
-  },
-  {
-    title: "Mission Inspired",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/mission.png",
-    desp: "InspireEd Hacks is a nationwide initiative organized by the student-for-student nonprofit, Mission InspirEd, challenging students ages 10-18 to synthesize their creativity and coding experience into a product beneficial to the educational community.",
-  },
-  {
-    title: "Hack3",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/hack3.png",
-    desp: "Hack3 2021 is an upcoming global online hackathon that aims to enable high school students to learn computer science, with a broader message of (everyone can code) targeting underrepresented demographics to improve diversity and inclusion of individuals in the computer technology space.",
-  },
-  {
-    title: "JITHack",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/jithack.png",
-    desp: "JITHack is an upcoming virtual design and development hackathon organized by the students of Jyothy Institute of Technology, Bengaluru on June 26, 2021, where participants have 24 hours to build something incredible.",
-  },
-  {
-    title: "Recess Hacks",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/recess.png",
-    desp: "Recess Hacks is an upcoming hackathon for high school students in the Greater Toronto Area that strives to encourage innovation among high school students and get involved in technology.",
-  },
-  {
-    title: "Citro Tech",
-    subtitle: "Mentor",
-    image: "assets/images/experience-page/citro.png",
-    desp: "Citro Hacks 2021 is an upcoming all-inclusive online hackathon organized by Citro Tech, a student led organization dedicated to empowering minorities in the tech field</li>",
-  },
-  {
-    title: "NHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/nhacks.png",
-    desp: "NHacks VI was a hackathon that aimed to encourage students everywhere to develop skills in computer science by creating tangible products that they're proud of. It initially started as Michigan's largest high school hackathon.",
+    title: "ESP8266 Tips & Tricks",
+    subtitle: "ACROBOTIC",
+    image: "assets/images/experience-page/esp8266.jpg",
+    desp: "Codebase and accompanying YouTube series with step-by-step guides for practical uses of the ESP8266 SoC.",
   },
 ];
 
@@ -215,7 +152,7 @@ const showCards3 = () => {
           <div class="blog-slider__title">${title}</div>
           <span class="blog-slider__code">${subtitle}</span>
           <div class="blog-slider__text">${desp}</div>
-          <a href="#" class="blog-slider__button">Read More</a>   
+          <a href="https://github.com/datasith" class="blog-slider__button">Read More</a>   
         </div>
       </div>
       `)
